6416fb340febbf646653757954d84af48e581089
Golu Choudhary <103061012+Golu7667@users.noreply.github.com>
2023-11-05 11:12:32 +0000

    add sever folder

---
+++ server/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mysql = require('mysql2/promise');
+const app = express();
+
+app.use(express.json());
+
+const dbConfig = {
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'Admin',
+  database: 'your-mysql-database',
+};
+
+const pool = mysql.createPool(dbConfig);
+
+// Create a new post
+app.post('/api/posts', async (req, res) => {
+  const { title, content } = req.body;
+  const connection = await pool.getConnection();
+  try {
+    const [result] = await connection.query(
+      'INSERT INTO posts (title, content) VALUES (?, ?)',
+      ["title", "content"]
+    );
+    res.json({ id: result.insertId });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error creating the post.' });
+  } finally {
+    connection.release();
+  }
+});
+
+// Retrieve all posts
+app.get('/api/posts', async (req, res) => {
+  const [rows] = await pool.query('SELECT * FROM posts');
+  res.json(rows);
+});
+
+// Retrieve a single post by ID
+app.get('/api/posts/:id', async (req, res) => {
+  const { id } = req.params;
+  const [row] = await pool.query('SELECT * FROM posts WHERE id = ?', [id]);
+  if (row.length === 0) {
+    res.status(404).json({ error: 'Post not found' });
+  } else {
+    res.json(row[0]);
+  }
+});
+
+app.listen(8000, () => {
+  console.log('Server is running on port 3001');
+});

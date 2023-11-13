c516bb923810e0e584c4884263dbd52c7abc08b8
unknown <goluchoudhary7808@gmail.com>
2023-11-13 16:26:14 +0530

    home page updated

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import {Input} from '@chakra-ui/react'
+import {Input,Center,Text,VStack,Button} from '@chakra-ui/react'
+
 
 
 function index() {
   return (
-    <div>
-    <Input />
-        
+    <Center w="100vw" >
+        <VStack>
+        <Text fontSize="3xl" fontWeight="bold">Enter Name</Text>
+       <Input w="300px" placeholder="Enter Name"/>
+       <Button>Save</Button>
+       </VStack>
 
-    </div>
+    </Center>
   )
 }
 
